ab8906d097e34ffb542fa6b193a131a6c10f23b3
ryuto <ryu.i7133@gmail.com>
2022-02-28 22:18:37 +0900

    ポケモンを10匹ずつ表示するロジックを作った

--- src/pages/pokemon/index.tsx
+++ src/pages/pokemon/index.tsx
@@ -1,6 +1,7 @@
 import useSWR from 'swr';
 import axios from 'axios';
 import Image from 'next/image'
+import { useState } from 'react';
 
 
 const url = 'https://pokeapi.co/api/v2/pokemon';
@@ -24,12 +25,31 @@ const Pokemon = (props:{count:number}) => {
 };
 
 const PokemonList = () => {
-	const data = [1, 3, 21, 64, 431, 545, 12, 35, 345];
+	const [pokemonNumber,setPokemonNumber] = useState<number>(10)
+	const pokemonCount = [...Array(10)];
+
+	const onNextPage = () => {
+		const newNumber = pokemonNumber + 10
+		setPokemonNumber(newNumber)
+	}
+
+	const onPrevPage = () => {
+		const newNumber = pokemonNumber - 10
+		setPokemonNumber(newNumber)
+	}
+
+	const onResetNumber = () => {
+		setPokemonNumber(1)
+	}
+	
 	return (
 		<div>
-			{data.map((d) => (
-				<Pokemon key={d} count={d} />
+			{pokemonCount.map((d,index) => (
+				<Pokemon key={index} count={index+pokemonNumber} />
 			))}
+			<button onClick={onResetNumber}>最初に戻る</button>
+			<button onClick={onPrevPage}>前のページ</button>
+			<button onClick={onNextPage}>次のページ</button>
 		</div>
 	);
 };
