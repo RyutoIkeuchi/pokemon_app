00aa7b2ce416b21c711eff5df830565b794c1697
ryuto <ryu.i7133@gmail.com>
2022-02-28 23:10:15 +0900

    ポケモンを20個ずつ取得するように変更、ボタンのUIを整えた、最初のページの時は押せないようにした

--- src/pages/pokemon/index.tsx
+++ src/pages/pokemon/index.tsx
@@ -3,16 +3,16 @@ import { Layout } from '../../components/template/Layout';
 import PokemonCard from '../../components/template/PokemonCard';
 
 const PokemonList = () => {
-	const [pokemonNumber, setPokemonNumber] = useState<number>(10);
-	const pokemonCount = [...Array(10)];
+	const [pokemonNumber, setPokemonNumber] = useState<number>(1);
+	const pokemonCount = [...Array(20)];
 
 	const onNextPage = () => {
-		const newNumber = pokemonNumber + 10;
+		const newNumber = pokemonNumber + 20;
 		setPokemonNumber(newNumber);
 	};
 
 	const onPrevPage = () => {
-		const newNumber = pokemonNumber - 10;
+		const newNumber = pokemonNumber - 20;
 		setPokemonNumber(newNumber);
 	};
 
@@ -22,13 +22,38 @@ const PokemonList = () => {
 
 	return (
 		<Layout>
-			<div>
+			<button
+				onClick={onResetNumber}
+				className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+			>
+				最初に戻る
+			</button>
+			<div className="grid grid-cols-4 gap-4 mb-10">
 				{pokemonCount.map((d, index) => (
 					<PokemonCard key={index} count={index + pokemonNumber} />
 				))}
-				<button onClick={onResetNumber}>最初に戻る</button>
-				<button onClick={onPrevPage}>前のページ</button>
-				<button onClick={onNextPage}>次のページ</button>
+			</div>
+			<div className="text-center mb-6">
+				<div className="inline-flex">
+					<button
+						onClick={onPrevPage}
+						disabled={pokemonNumber == 1}
+						className={
+							`bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-l' +
+							${pokemonNumber === 1
+								? 'cursor-not-allowed opacity-50'
+								: 'hover:bg-gray-400'}`
+						}
+					>
+						前のページ
+					</button>
+					<button
+						onClick={onNextPage}
+						className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+					>
+						次のページ
+					</button>
+				</div>
 			</div>
 		</Layout>
 	);
