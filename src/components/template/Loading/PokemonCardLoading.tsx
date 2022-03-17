c6937c7c203bbcfda2df638fb1e0628f6d90bd26
ryuto <ryu.i7133@gmail.com>
2022-03-18 08:35:05 +0900

    ローディング処理を別ファイルに移す

---
+++ src/components/template/Loading/PokemonCardLoading.tsx
@@ -0,0 +1,18 @@
+export const PokemonCardLoading = () => {
+  return (
+		<div className="border border-blue-300 shadow rounded-md p-4 w-ful">
+			<div className="float-right h-4 bg-slate-200 w-1/12"></div>
+			<div className="w-36 h-36 mx-auto rounded-full bg-slate-200"></div>
+			<div className="rounded-full bg-slate-200 h-20 w-20 mb-2"></div>
+			<div className="rounded-xl border-2 p-4 border-blue-300 ">
+				<div className="grid grid-cols-3 gap-4">
+					<div className="h-8 mb-2 bg-slate-200 rounded col-span-3"></div>
+				</div>
+				<div className="flex justify-start">
+					<div className="bg-slate-200 px-2 h-6 w-20 rounded mr-2"></div>
+					<div className="bg-slate-200 px-2 h-6 w-20 rounded mr-2"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/components/template/PokemonCard.tsx
+++ src/components/template/PokemonCard.tsx
@@ -2,6 +2,7 @@ import useSWRImmutable from 'swr/immutable';
 import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
+import { PokemonCardLoading } from './Loading/PokemonCardLoading';
 
 const url = 'https://pokeapi.co/api/v2/pokemon';
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
@@ -9,23 +10,7 @@ const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 const PokemonCard = (props: { count: number }) => {
 	const { data, error } = useSWRImmutable(`${url}/${props.count}`, fetcher);
 
-	if (!data && !error)
-		return (
-			<div className="border border-blue-300 shadow rounded-md p-4 w-ful">
-				<div className="float-right h-4 bg-slate-200 w-1/12"></div>
-				<div className="w-36 h-36 mx-auto rounded-full bg-slate-200"></div>
-				<div className="rounded-full bg-slate-200 h-20 w-20 mb-2"></div>
-				<div className="rounded-xl border-2 p-4 border-blue-300 ">
-					<div className="grid grid-cols-3 gap-4">
-						<div className="h-8 mb-2 bg-slate-200 rounded col-span-3"></div>
-					</div>
-					<div className="flex justify-start">
-						<div className="bg-slate-200 px-2 h-6 w-20 rounded mr-2"></div>
-						<div className="bg-slate-200 px-2 h-6 w-20 rounded mr-2"></div>
-					</div>
-				</div>
-			</div>
-		);
+	if (!data && !error) return <PokemonCardLoading />;
 	if (error) return <h2>エラーだよ</h2>;
 
 	const mainImageLoader = ({ src }: { src: string }) => {
